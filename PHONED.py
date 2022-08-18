42e8446eb081c042e96db941c06157c56e79603d
TIOX-88 <109265909+TIOX-88@users.noreply.github.com>
2022-08-18 23:06:34 +0600

    Create PHONED.py

---
+++ PHONED.py
@@ -0,0 +1,18 @@
+import pyfiglet
+import phonenumbers
+from phonenumbers import timezone,geocoder,carrier
+
+text = pyfiglet.figlet_format("EHCG")
+number=input("Enter Your NO. With Country Code +(: ")
+phone=phonenumbers.parse(number)
+time=timezone.time_zones_for_number(phone)
+car=carrier.name_for_number(phone,"en")
+reg=geocoder.description_for_number(phone,"en")
+
+
+
+print(text)
+print(phone)
+print(time)
+print(car)
+print(reg)
